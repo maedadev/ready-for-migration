6dc3df1188209e39b73e5a4fd4892be0e69c2bb8
y-matsuda <matsuda@lab.acs-jp.com>
2022-07-28 12:55:44 +0900

    refs #3

--- test/controllers/ready_for_migration/health_controller_test.rb
+++ test/controllers/ready_for_migration/health_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class ReadyForMigration::HealthControllerTest < ActionDispatch::IntegrationTest
   def test_readiness
-    get '/healthz'
+    get '/'
     assert_response :success
   end
 end
