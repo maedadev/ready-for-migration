4690ea4b598140b532ee148d5512fdd95e5d5d43
Aoyama <aoyama@netlab.jp>
2023-01-18 18:18:39 +0900

    503 Service Unavailable を応答させるための仕掛け

--- lib/ready/for/migration/engine.rb
+++ lib/ready/for/migration/engine.rb
@@ -2,6 +2,13 @@ module Ready
   module For
     module Migration
       class Engine < ::Rails::Engine
+
+        initializer 'ready-for-migration.initialize_cache' do
+          # When lanched as server, clear cache.
+          if defined? Rails::Server || defined? PhusionPassenger
+            Ready::For::Migration::HealthActionInspectable.cache.clear
+          end
+        end
       end
     end
   end
